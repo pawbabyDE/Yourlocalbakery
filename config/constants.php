34e072b35f6d8215bd3685fb59aecb95916523b5
pawbabyDE <radek.ogielautex@gmail.com>
2023-11-05 20:10:55 +0100

    again trying anything

--- config/constants.php
+++ config/constants.php
@@ -8,7 +8,7 @@
     define('LOCALHOST', 'localhost'); //localhost = adres potencjalnego serwera
     define('DB_USERNAME', 'root'); //usr do DB
     define('DB_PASSWORD', ''); //pwd do DB 
-    define('DB_NAME', 'onlinefoodorder'); //nazwa DB
+    define('DB_NAME', 'yrlb'); //nazwa DB
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die("Connection failed: " . mysqli_error($conn)); //łączenie z bazą danych, albo przejdzie albo łącze zdycha
     $db_select = mysqli_select_db($conn, DB_NAME) or die("Database selection failed: " . mysqli_error($conn)); //Wybieranie bazy danych. jak nie znajdzie tej z linijki 11 to połączenie zdycha
